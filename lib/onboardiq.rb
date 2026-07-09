bce6d7d768941ce17c366e2451f45f3e8caa1ae3
OpenInspect <open-inspect@noreply.github.com>
2026-07-09 13:01:34 +0000

    Fix critical correctness bugs in OnboardIQ API methods

--- lib/onboardiq.rb
+++ lib/onboardiq.rb
@@ -10,36 +10,37 @@ class OnboardIQ
 
   def initialize(hash)
     @token = hash[:token]
+    raise ArgumentError, 'token is required' unless @token
     @version = DEFAULT_API_VERSION
     self.class.default_params api_token: @token
   end
 
   def list_applicants
     response = self.class.get("/#{@version}/applicants")
-    JSON.parse(response.body)
+    return JSON.parse(response.body)
 
-    rescue
+    rescue StandardError => e
       nil
   end
 
   def create_applicant(attrs)
     response = self.class.post("/#{@version}/applicants", body: attrs)
-    JSON.parse(response.body)
-    rescue
+    return JSON.parse(response.body)
+    rescue StandardError => e
       nil
   end
 
   def update_applicant(applicant_key, attrs)
     response = self.class.put("/#{@version}/applicants/#{applicant_key}", body: attrs)
-    JSON.parse(response.body)
-    rescue
+    return JSON.parse(response.body)
+    rescue StandardError => e
       nil
   end
 
   def get_applicant(applicant_key)
     response = self.class.get("/#{@version}/applicants/#{applicant_key}")
-    JSON.parse(response.body)
-    rescue
+    return JSON.parse(response.body)
+    rescue StandardError => e
       nil
   end
 
